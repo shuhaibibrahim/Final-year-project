3e73a6da2f808396db3bfb1cd74feea2f2993d71
shuhaibibrahim <shuhaib.ibrahim2000@gmail.com>
2022-05-12 13:58:48 +0530

    safe commit

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const users=[
     {
         username: "tve18cs061",
         password: "1234",
-        roles:["admin","warden"]
+        roles:["admin", "staff advisor", "hod", "warden", "hosteloffice"]
     }
 ]
 
