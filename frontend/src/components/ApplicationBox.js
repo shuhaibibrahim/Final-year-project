8e218481442d5ea4715c76a34929618bf2238212
N Athul Kumar <athulnvinod@gmail.com>
2022-05-27 12:08:11 +0530

    athul changes

--- frontend/src/components/ApplicationBox.js
+++ frontend/src/components/ApplicationBox.js
@@ -1,16 +1,16 @@
 import {motion} from 'framer-motion'
 import {useState} from 'react'
 import FormDialog from './FormDialog'
-export default function ApplicationBox({applicationTitle,description,fields}){
+export default function ApplicationBox({applicationTitle,description,fields,certificateId}){
     const [open,setOpen]=useState(false)
     return(
         <motion.div  whileHover={{scale:1.02}} className="border border-stone-700 bg-sky-100 p-3 rounded-md">
-            <h1 className='text-black font-semibold text-md'>{applicationTitle}</h1>
+            <h1 className='text-black font-semibold text-md capitalize'>{applicationTitle}</h1>
             <h2 className='text-gray-600 text-sm'>{description}</h2>
             <div className="flex items-center justify-end">
                 <button onClick={()=>{setOpen(true)}} className="submit-button-black">Apply</button>
             </div>
-            <FormDialog open={open} setOpen={setOpen} field={fields} modalHeading={applicationTitle}/>
+            <FormDialog open={open} setOpen={setOpen} field={fields} modalHeading={applicationTitle} certificateId={certificateId}/>
         </motion.div>
     )
 }
--- frontend/src/components/ApplicationBoxes.js
+++ frontend/src/components/ApplicationBoxes.js
@@ -1,66 +1,79 @@
+import { useState,useEffect } from 'react'
+import axios from "axios"
+import { baseUrl } from "../baseUrl"
 import ApplicationBox from "./ApplicationBox"
 function ApplicationBoxes() {
-    const applications=[
-        {
-            title:"Inmate Certificate",
-            description:"",
-            fields:`{
-                "Purpose": {
-                              "tag":"textarea",
-                              "label":"Purpose",
-                              "name":"purpose",
-                              "type":"text"
-                            },
-                "Remarks": {
-                              "tag":"textarea",
-                              "label":"Remarks",
-                              "name":"remarks",
-                              "type":"text"
-                            }
-            }`   
-        },
-        {
-            title:"Fee Structure Certificate",
-            description:"",
-            fields:`{
-                "Purpose": {
-                            "tag":"textarea",
-                            "label":"Purpose",
-                            "name":"purpose",
-                            "type":"text"
-                            },
-                "Remarks": {
-                            "tag":"textarea",
-                            "label":"Purpose",
-                            "name":"purpose",
-                            "type":"text"
-                            }
-            }`   
-        },
-        {
-            title:"No Due Certificate",
-            description:"",
-            fields:`{
-                "Purpose": {
-                                "tag":"textarea",
-                                "label":"Purpose",
-                                "name":"purpose",
-                                "type":"text"
-                            },
-                "Remarks": {
-                                "tag":"textarea",
-                                "label":"Purpose",
-                                "name":"purpose",
-                                "type":"text"
-                            }
-            }`   
-        }
-    ]
+
+    useEffect(() => {
+      axios.get(`${baseUrl}/inmate/formtemplate`)
+      .then(res=>{
+          console.log(res.data)
+          setApplications(res.data)
+      })
+    }, [])
+    
+    // const applications=[
+    //     {
+    //         title:"Inmate Certificate",
+    //         description:"",
+    //         fields:`{
+    //             "Purpose": {
+    //                           "tag":"textarea",
+    //                           "label":"Purpose",
+    //                           "name":"purpose",
+    //                           "type":"text"
+    //                         },
+    //             "Remarks": {
+    //                           "tag":"textarea",
+    //                           "label":"Remarks",
+    //                           "name":"remarks",
+    //                           "type":"text"
+    //                         }
+    //         }`   
+    //     },
+    //     {
+    //         title:"Fee Structure Certificate",
+    //         description:"",
+    //         fields:`{
+    //             "Purpose": {
+    //                         "tag":"textarea",
+    //                         "label":"Purpose",
+    //                         "name":"purpose",
+    //                         "type":"text"
+    //                         },
+    //             "Remarks": {
+    //                         "tag":"textarea",
+    //                         "label":"Purpose",
+    //                         "name":"purpose",
+    //                         "type":"text"
+    //                         }
+    //         }`   
+    //     },
+    //     {
+    //         title:"No Due Certificate",
+    //         description:"",
+    //         fields:`{
+    //             "Purpose": {
+    //                             "tag":"textarea",
+    //                             "label":"Purpose",
+    //                             "name":"purpose",
+    //                             "type":"text"
+    //                         },
+    //             "Remarks": {
+    //                             "tag":"textarea",
+    //                             "label":"Purpose",
+    //                             "name":"purpose",
+    //                             "type":"text"
+    //                         }
+    //         }`   
+    //     }
+    // ]
+    const [applications,setApplications]=useState([])
     return (
         <div className="grid grid-cols-3 gap-3 w-11/12">
             {applications.map((item,index)=>{
                 return(
-                    <ApplicationBox key={index} applicationTitle={item.title} description={item.description} fields={item.fields}/>
+                    <ApplicationBox key={index} applicationTitle={item.name} fields={item.application_template} certificateId={item.certificate_id}/>
                 )
             })}
         </div>
